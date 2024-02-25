205f1a5095fff8763d22be4b9f54c6e15810c77d
Samuel Custódio <samuel.rodrigues@aluno.unievangelica.edu.br>
2024-02-25 18:15:25 -0300

    #add

---
+++ backend/schemas/index.js
@@ -0,0 +1,19 @@
+import testimonials from './testimonials'
+import about from './abouts'
+import skills from './skills'
+import experiences from './experiences'
+import brands from './brands'
+import contact from './contact'
+import works from './works'
+import workExperience from './workExperience'
+
+export const schemaTypes = [
+    testimonials,
+    about,
+    skills,
+    experiences,
+    brands,
+    contact,
+    works,
+    workExperience
+]
